6b273a9832164d229db53b182946b1b511511f44
jack19931993 <j7139107@yahoo.com.tw>
2021-09-27 17:55:01 +0800

    箭頭 banner

--- Portfolio/js/script.js
+++ Portfolio/js/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 	//Check to see if the window is top if not then display button
 	$(window).scroll(function(){
-		if ($(this).scrollTop() > 200) {
+		if ($(this).scrollTop() > 800) {
 			$('.btn_top').fadeIn();
 		} else {
 			$('.btn_top').fadeOut();
@@ -14,3 +14,19 @@ $(document).ready(function(){
 	});
 	
 });
+
+//Scroll_Smooth.js
+$(function() {
+	$('a[href*="#"]:not([href="#"])').click(function() {
+	  if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+		var target = $(this.hash);
+		target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+		if (target.length) {
+		  $('html, body').animate({
+			scrollTop: target.offset().top
+		  }, 1000);
+		  return false;
+		}
+	  }
+	});
+  });
